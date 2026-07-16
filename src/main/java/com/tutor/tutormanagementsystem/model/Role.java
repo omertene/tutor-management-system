9b5a3cd9f10860a1d406a60669f26e532ee80822
omertene <omer.tenenbaum4@gmail.com>
2026-07-16 22:54:26 +0300

    feat: add basic User entity and Role enum under model package

---
+++ src/main/java/com/tutor/tutormanagementsystem/model/Role.java
@@ -0,0 +1,6 @@
+package com.tutor.tutormanagementsystem.model;
+
+public enum Role {
+    TEACHER,
+    STUDENT
+}
---
+++ src/main/java/com/tutor/tutormanagementsystem/model/User.java
@@ -0,0 +1,35 @@
+package com.tutor.tutormanagementsystem.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false, unique = true) // לבדוק במקרה של הורה עם אותו מייל לשני תלמידים
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(name = "full_name", nullable = false)
+    private String fullName;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+}
