6bebe27e41b32566437672c616f00622a7aca465
omertene <omer.tenenbaum4@gmail.com>
2026-08-23 18:38:56 +0300

    fix: correct misleading parameter names

--- src/main/java/com/tutor/tutormanagementsystem/repository/ScheduleOverrideRepository.java
+++ src/main/java/com/tutor/tutormanagementsystem/repository/ScheduleOverrideRepository.java
@@ -10,5 +10,5 @@ import java.util.List;
 public interface ScheduleOverrideRepository extends JpaRepository<ScheduleOverride, Long> {
 
     List<ScheduleOverride> findAllByDate(LocalDate date);
-    List<ScheduleOverride> findAllByDateAndStartTimeLessThanAndEndTimeGreaterThan(LocalDate date, LocalTime startTime, LocalTime endTIme);
+    List<ScheduleOverride> findAllByDateAndStartTimeLessThanAndEndTimeGreaterThan(LocalDate date, LocalTime endTime, LocalTime startTime);
 }
